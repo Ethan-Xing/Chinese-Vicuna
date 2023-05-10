c72946dfc3673160e25a91e5e58e1f1110557742
Ethan.Xing <xingyi@bonc.com.cn>
2023-05-10 17:18:15 +0800

    修改final checkpoint的逻辑

--- finetune.py
+++ finetune.py
@@ -104,6 +104,7 @@ if args.resume_from_checkpoint:
         if os.path.exists(checkpoint_name):
             os.rename(checkpoint_name, pytorch_bin_path)
             warnings.warn("The file name of the lora checkpoint'adapter_model.bin' is replaced with 'pytorch_model.bin'")
+            checkpoint_name = pytorch_bin_path
         else:
             args.resume_from_checkpoint = (
                 None  # So the trainer won't try loading its state
